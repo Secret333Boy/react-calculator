e5dc2c88d1f73dba530a1553555e9b3cf77bf26c
Eugene <pashkovskiy.eugen@gmail.com>
2022-07-05 22:24:33 +0300

    Refactored performOperators function

--- src/utils/performOperators.js
+++ src/utils/performOperators.js
@@ -1,25 +1,21 @@
 const performOperators = (numbers, operators) => {
   for (let i = 0; i < operators.length; i++) {
-    if (operators[i] === 'x') {
-      numbers[i] *= numbers[i + 1];
-      numbers.splice(i + 1, 1);
-      operators.splice(i, 1);
-      i--;
-    } else if (operators[i] === '÷') {
-      numbers[i] /= numbers[i + 1];
+    if (['x', '÷'].includes(operators[i])) {
+      numbers[i] =
+        operators[i] === 'x'
+          ? numbers[i] * numbers[i + 1]
+          : numbers[i] / numbers[i + 1];
       numbers.splice(i + 1, 1);
       operators.splice(i, 1);
       i--;
     }
   }
   for (let i = 0; i < operators.length; i++) {
-    if (operators[i] === '+') {
-      numbers[i] += numbers[i + 1];
-      numbers.splice(i + 1, 1);
-      operators.splice(i, 1);
-      i--;
-    } else if (operators[i] === '-') {
-      numbers[i] -= numbers[i + 1];
+    if (['+', '-'].includes(operators[i])) {
+      numbers[i] =
+        operators[i] === '+'
+          ? numbers[i] + numbers[i + 1]
+          : numbers[i] - numbers[i + 1];
       numbers.splice(i + 1, 1);
       operators.splice(i, 1);
       i--;
